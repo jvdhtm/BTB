629f32cad1e6e018aae54e5ea4e7cae3183fa8d9
Unknown <javad.hatami@sigma.se>
2018-02-01 13:56:05 +0100

    changes

---
+++ assets/projects/veidec/static/scripts/hsa.js
@@ -0,0 +1,10 @@
+
+var hsVars = {
+    ticks: 1517489203797,
+    page_id: 5544090198,
+    content_group_id: 0,
+    portal_id: 4011209,
+    app_hs_base_url: "https://app.hubspot.com",
+    language: "en",
+    analytics_page_type: "standard-page"
+}
